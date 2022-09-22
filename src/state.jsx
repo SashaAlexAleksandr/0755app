6d4a4c817aee1cbbc3744f5fcb265529dfda486b
Aleksandr Skvortsov <sasha-scvor@yandex.ru>
2022-09-22 10:04:04 +0300

    22.09.2022 use state.jsx for Profile

---
+++ src/state.jsx
@@ -0,0 +1,13 @@
+
+const user = {
+    name:"Иван",
+    lastname: "Иванов",
+    email: "ivan@ya.ru",
+    id: "1",
+    about: "Тут я рассказываю историю своей жизни и о своих увлечениях...",
+    avatar: "/public/r5.jpg",
+};
+
+export function getUser() {
+    return user;
+}
